efb952a8f3dda73dcc1f0a6f1b115a7780dc704e
Shawn Yates <sdy0002@auburn.edu>
2022-03-30 13:57:32 -0700

    final form this works when given a sequence that has an inframe stop codon

--- translate_orf.py
+++ translate_orf.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python3
 
-
 def main():
+    from find_orf import find_first_orf
+    from find_orf import parse_sequence_from_path
+    from translate import translate_sequence
     import argparse
 
     # Create a command-line parser object
@@ -57,7 +59,14 @@ def main():
     orf = find_first_orf(sequence = sequence,
             start_codons = args.start_codon,
             stop_codons = args.stop_codon)
-    sys.stdout.write('{}\n'.format(orf))
 
+
+    # Find first orf and translate using "find_first_orf" and "translate_sequence" functions
+    first_orf_trans = translate_sequence(orf, genetic_code)
+    sys.stdout.write('{}\n'.format(first_orf_trans))
+    return(first_orf_trans)
 
 if __name__ == '__main__':
+    import sys
+    genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P', 'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C', 'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S', 'AGG': 'R', 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*', 'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I', 'GCA': 'A', 'CUU': 'L', 'GGC': 'G', 'AUG': 'M', 'CUG': 'L', 'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A', 'AAA': 'K', 'AAG': 'K', 'CAA': 'Q', 'UUU': 'F', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I', 'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F'}
+    main()
